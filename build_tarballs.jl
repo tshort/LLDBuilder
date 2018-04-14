edd9e739f4137cabb1889ac2907199a246c89da6
tshort <tshort.rlists@gmail.com>
2018-04-14 12:55:55 -0400

    Use a static build for windows

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -11,9 +11,10 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir
 cd wabt-1.0.0/
+if [ $target = "x86_64-w64-mingw32" ] || [ $target = "i686-w64-mingw32" ]; then
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain -DCMAKE_BUILD_TYPE=gcc-release -DBUILD_TESTS=OFF -DCMAKE_CXX_FLAGS="-static -no-pie" -DCMAKE_C_FLAGS="-static -no-pie"
+else
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain -DCMAKE_BUILD_TYPE=gcc-release -DBUILD_TESTS=OFF
-if [ $target = "i686-w64-mingw32" ]; then
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain -DCMAKE_BUILD_TYPE=gcc-i386-release -DBUILD_TESTS=OFF
 fi
 
 make -j8
@@ -23,29 +24,14 @@ if [ $target = "x86_64-w64-mingw32" ]; then
   echo hello
 fi
 
-
-if [ $target = "x86_64-unknown-freebsd11.1" ]; then
-cd $WORKSPACE/srcdir
-ls
-cd wabt-1.0.0/
-ls -l
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain -DCMAKE_BUILD_TYPE=clang-release -DBUILD_TESTS=OFF
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain -DCMAKE_BUILD_TYPE=gcc-release -DBUILD_TESTS=OFF
-make --version
-ls /opt/$target
-ls -l /opt/$target
-ls -l /opt/$target/x86_64-unknown-freebsd11.1/
-
-ls -l /opt/$target/x86_64-unknown-freebsd11.1/
-ls -l /opt/$target/
-
-fi
-
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
+    BinaryProvider.Windows(:x86_64),
+    BinaryProvider.Windows(:i686),
+    BinaryProvider.MacOS(:x86_64, :blank_libc, :blank_abi),
     BinaryProvider.Linux(:i686, :glibc, :blank_abi),
     BinaryProvider.Linux(:x86_64, :glibc, :blank_abi),
     BinaryProvider.Linux(:aarch64, :glibc, :blank_abi),
@@ -54,8 +40,7 @@ platforms = [
     BinaryProvider.Linux(:i686, :musl, :blank_abi),
     BinaryProvider.Linux(:x86_64, :musl, :blank_abi),
     BinaryProvider.Linux(:aarch64, :musl, :blank_abi),
-    BinaryProvider.Linux(:armv7l, :musl, :eabihf),
-    BinaryProvider.MacOS(:x86_64, :blank_libc, :blank_abi)
+    BinaryProvider.Linux(:armv7l, :musl, :eabihf)
 ]
 
 # The products that we will ensure are always built
